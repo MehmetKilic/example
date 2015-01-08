4fe5a333c591d6de91552789b3afa1997d66d1cc
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2015-01-08 13:07:22 +0200

    Method added

---
+++ src/Ozziest/Example/Example.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Ozziest\Example;
+
+class Example {
+
+	/**
+	 * Get title
+	 *
+	 * @param  string 		$text 
+	 * @return string
+	 */
+	public function title($text)
+	{
+        return trim(
+        	mb_convert_case(
+        		str_replace(
+	        		[' I',' ı', ' İ', ' i'],
+	            	[' I',' I',' İ',' İ'],
+	            	$text
+	            ),
+            	MB_CASE_TITLE, 
+            	"UTF-8"
+            )
+        );
+	}
+
+}
